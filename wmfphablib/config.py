fbb20fe7e9c8ba8cac516495bdda0449a617bebe
Mathieu Tarral <mathieu.tarral@gmail.com>
2016-02-03 22:57:39 +0100

    disable config parsing

--- wmfphablib/config.py
+++ wmfphablib/config.py
@@ -6,55 +6,55 @@ import time
 
 cfile = '/etc/phabtools.conf'
 
-import ConfigParser
-parser = ConfigParser.SafeConfigParser()
-parser.read(cfile)
-dbhost = parser.get('general', 'dbhost')
-dbslave = parser.get('general', 'dbslave')
-file_upload_timeout = int(parser.get('general', 'file_upload_timeout'))
-parser_mode = 'phmanifest'
-phmanifest_user = parser.get(parser_mode, 'user')
-phmanifest_passwd = parser.get(parser_mode, 'passwd')
-parser_mode = 'phuser'
-phuser_user = parser.get(parser_mode, 'user')
-phuser_passwd = parser.get(parser_mode, 'passwd')
-parser_mode = 'fabmigrate'
-fabmigrate_db = parser.get(parser_mode, 'db')
-fabmigrate_user = parser.get(parser_mode, 'user')
-fabmigrate_passwd = parser.get(parser_mode, 'passwd')
-fab_multi = int(parser.get(parser_mode, 'multi'))
-fab_limit = int(parser.get(parser_mode, 'limit'))
-parser_mode = 'phab'
-phab_user = parser.get(parser_mode, 'username')
-phab_cert = parser.get(parser_mode, 'certificate')
-phab_host = parser.get(parser_mode, 'host')
-parser_mode = 'bz'
-Bugzilla_url = parser.get(parser_mode, 'url')
-Bugzilla_login = parser.get(parser_mode, 'Bugzilla_login')
-Bugzilla_password = parser.get(parser_mode, 'Bugzilla_password')
-parser_mode = 'bzmigrate'
-bzmigrate_db = parser.get(parser_mode, 'db')
-bzmigrate_user = parser.get(parser_mode, 'user')
-bzmigrate_passwd = parser.get(parser_mode, 'passwd')
-bz_createmulti = int(parser.get(parser_mode, 'create_multi'))
-bz_fetchmulti = int(parser.get(parser_mode, 'fetch_multi'))
-bz_updatelimit = int(parser.get(parser_mode, 'update_limit'))
-bz_updatemulti = int(parser.get(parser_mode, 'update_multi'))
-bz_update_lookback = int(parser.get(parser_mode, 'update_lookback'))
-bz_populate_multi = int(parser.get(parser_mode, 'populate_multi'))
-bz_security = parser.get(parser_mode, 'security')
-parser_mode = 'rtmigrate'
-rtmigrate_db = parser.get(parser_mode, 'db')
-rtmigrate_user = parser.get(parser_mode, 'user')
-rtmigrate_passwd = parser.get(parser_mode, 'passwd')
-rt_createmulti = int(parser.get(parser_mode, 'create_multi'))
-rt_fetchmulti = int(parser.get(parser_mode, 'fetch_multi'))
-rt_updatelimit = int(parser.get(parser_mode, 'update_limit'))
-rt_updatemulti = int(parser.get(parser_mode, 'update_multi'))
-parser_mode = 'rt'
-rt_url = parser.get(parser_mode, 'url')
-rt_login = parser.get(parser_mode, 'login')
-rt_passwd = parser.get(parser_mode, 'passwd')
+# import ConfigParser
+# parser = ConfigParser.SafeConfigParser()
+# parser.read(cfile)
+# dbhost = parser.get('general', 'dbhost')
+# dbslave = parser.get('general', 'dbslave')
+# file_upload_timeout = int(parser.get('general', 'file_upload_timeout'))
+# parser_mode = 'phmanifest'
+# phmanifest_user = parser.get(parser_mode, 'user')
+# phmanifest_passwd = parser.get(parser_mode, 'passwd')
+# parser_mode = 'phuser'
+# phuser_user = parser.get(parser_mode, 'user')
+# phuser_passwd = parser.get(parser_mode, 'passwd')
+# parser_mode = 'fabmigrate'
+# fabmigrate_db = parser.get(parser_mode, 'db')
+# fabmigrate_user = parser.get(parser_mode, 'user')
+# fabmigrate_passwd = parser.get(parser_mode, 'passwd')
+# fab_multi = int(parser.get(parser_mode, 'multi'))
+# fab_limit = int(parser.get(parser_mode, 'limit'))
+# parser_mode = 'phab'
+# phab_user = parser.get(parser_mode, 'username')
+# phab_cert = parser.get(parser_mode, 'certificate')
+# phab_host = parser.get(parser_mode, 'host')
+# parser_mode = 'bz'
+# Bugzilla_url = parser.get(parser_mode, 'url')
+# Bugzilla_login = parser.get(parser_mode, 'Bugzilla_login')
+# Bugzilla_password = parser.get(parser_mode, 'Bugzilla_password')
+# parser_mode = 'bzmigrate'
+# bzmigrate_db = parser.get(parser_mode, 'db')
+# bzmigrate_user = parser.get(parser_mode, 'user')
+# bzmigrate_passwd = parser.get(parser_mode, 'passwd')
+# bz_createmulti = int(parser.get(parser_mode, 'create_multi'))
+# bz_fetchmulti = int(parser.get(parser_mode, 'fetch_multi'))
+# bz_updatelimit = int(parser.get(parser_mode, 'update_limit'))
+# bz_updatemulti = int(parser.get(parser_mode, 'update_multi'))
+# bz_update_lookback = int(parser.get(parser_mode, 'update_lookback'))
+# bz_populate_multi = int(parser.get(parser_mode, 'populate_multi'))
+# bz_security = parser.get(parser_mode, 'security')
+# parser_mode = 'rtmigrate'
+# rtmigrate_db = parser.get(parser_mode, 'db')
+# rtmigrate_user = parser.get(parser_mode, 'user')
+# rtmigrate_passwd = parser.get(parser_mode, 'passwd')
+# rt_createmulti = int(parser.get(parser_mode, 'create_multi'))
+# rt_fetchmulti = int(parser.get(parser_mode, 'fetch_multi'))
+# rt_updatelimit = int(parser.get(parser_mode, 'update_limit'))
+# rt_updatemulti = int(parser.get(parser_mode, 'update_multi'))
+# parser_mode = 'rt'
+# rt_url = parser.get(parser_mode, 'url')
+# rt_login = parser.get(parser_mode, 'login')
+# rt_passwd = parser.get(parser_mode, 'passwd')
 
 
 if __name__ == '__main__':
